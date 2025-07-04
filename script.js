8b7731009c7487505833064ab5eec5e3f8ea5c9a
Misha Zorin <zorin.mikhail.e@gmail.com>
2025-07-04 13:33:19 +0300

    js

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ let modal = document.getElementById('modal')
 let modal_about = document.getElementById('modal_about')
 let about = document.getElementById('about')
 let projects = document.getElementById('projects')
+let contacts = document.getElementById('contacts')
+let modal_contacts = document.getElementById('modal_contacts')
+
+function show_modal(m){
+    let modal_show = document.getElementsByClassName("modal_show")[0]
+    modal_show?.classList.remove("modal_show")
+    m.classList.add("modal_show")
+}
+
+
 projects.onclick = function () {
-    modal.style.display = 'block'
+    show_modal(modal)
 }
 about.onclick = function(){
-    modal_about.style.display = 'block'
+    show_modal(modal_about)
+    
+}
+contacts.onclick = function(){
+    show_modal(modal_contacts)
+    
 }
