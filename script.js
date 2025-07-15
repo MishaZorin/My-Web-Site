fbdd061603457cb65c54712fd7ce611ac40c05bd
MishaZorin <165201586+MishaZorin@users.noreply.github.com>
2025-07-15 19:51:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,23 +3,45 @@ let modal_about = document.getElementById('modal_about')
 let about = document.getElementById('about')
 let projects = document.getElementById('projects')
 let contacts = document.getElementById('contacts')
+let skills = document.getElementById('skills')
+let modal_skills = document.getElementById('modal_skills')
 let modal_contacts = document.getElementById('modal_contacts')
+let see = document.getElementById("see")
 
-function show_modal(m){
-    let modal_show = document.getElementsByClassName("modal_show")[0]
-    modal_show?.classList.remove("modal_show")
-    m.classList.add("modal_show")
-}
 
 
 projects.onclick = function () {
-    show_modal(modal)
+    modal.style.display = 'block'
+    modal_about.style.display = 'none'
+    modal_contacts.style.display = 'none'
+    modal_skills.style.display = 'none'
+    // show_modal(modal)
+}
+see.onclick = function () {
+    modal.style.display = 'block'
+    
+    // show_modal(modal)
 }
 about.onclick = function(){
-    show_modal(modal_about)
+    modal_about.style.display = 'block'
+    modal.style.display = 'none'
+    modal_contacts.style.display = 'none'
+    modal_skills.style.display = 'none'
+    // show_modal(modal_about)
     
 }
 contacts.onclick = function(){
-    show_modal(modal_contacts)
+    modal_contacts.style.display = 'block'
+    modal.style.display = 'none'
+    modal_about.style.display = 'none'
+    modal_skills.style.display = 'none'
+    // show_modal(modal_contacts)
     
+}
+skills.onclick = function(){
+    modal_skills.style.display = 'block'
+    modal_contacts.style.display = 'none'
+    modal.style.display = 'none'
+    modal_about.style.display = 'none'
+    // show_modal(modal_skills)
 }
